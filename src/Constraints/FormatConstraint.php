92934a7c31a50e86313ce741c7e3826bf3ce15e9
Thanos Korakas <thanoskorakas@gmail.com>
2022-08-29 16:15:34 +0000

    added all constraints

--- src/Constraints/FormatConstraint.php
+++ src/Constraints/FormatConstraint.php
@@ -8,31 +8,39 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 final class FormatConstraint implements ConstraintInterface
 {
-    private const FORMAT_MAP = [
-        'date' => Assert\Date::class,
-        'time' => Assert\Time::class,
-        'date-time' => Assert\DateTime::class,
-        'duration' => 'duration',
-        'regex' => 'regex',
-        'email' => Assert\Email::class,
-        'hostname' => Assert\Hostname::class,
-        'ipv4' => Assert\Ip::class,
-        'ipv6' => Assert\Ip::class,
-        'url' => Assert\Url::class,
-        'uuid' => Assert\Uuid::class,
-    ];
+    private array $formatMap;
+
+    public function __construct()
+    {
+        $this->formatMap = [
+            'date' => Assert\Date::class,
+            'time' => Assert\Time::class,
+            'date-time' => Assert\DateTime::class,
+            'regex' => Assert\Regex::class,
+            'email' => Assert\Email::class,
+            'hostname' => Assert\Hostname::class,
+            'ipv4' => Assert\Ip::class,
+            'ipv6' => Assert\Ip::class,
+            'url' => Assert\Url::class,
+            'uuid' => Assert\Uuid::class,
+        ];
+    }
 
     public function isApplicable(array $field): bool
     {
         return array_key_exists('type', $field)
             && $field['type'] === 'string'
             && array_key_exists('format', $field)
-            && array_key_exists($field['format'], self::FORMAT_MAP);
+            && array_key_exists($field['format'], $this->formatMap);
     }
 
     public function apply(array $field): array
     {
-        $formatter = self::FORMAT_MAP[$field['format']];
+        $formatter = $this->formatMap[$field['format']];
+
+        if (is_callable($formatter)) {
+            return $formatter($field);
+        }
 
         return [new $formatter];
     }
